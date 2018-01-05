366fe0ffc0ebdac58ebe5c81709f86e26059be67
James Ravenscroft <james.ravenscroft@filament.uk.com>
2018-01-05 13:05:46 +0000

    added new models for author and paper

--- flaskapp/model/__init__.py
+++ flaskapp/model/__init__.py
@@ -13,11 +13,53 @@ class NewsArticle(db.Model):
 
     title = db.Column(db.String(255))
 
-    text = db.Column("content", db.Text(collation="utf8_general_ci") )
+    text = db.Column("content", db.Text(collation="utf8_general_ci"))
+
+
+paper_authors = db.Table('paper_authors',
+                         db.Column('paper_id', db.Integer,
+                                   db.ForeignKey('papers.paper_id'),
+                                   primary_key=True),
+                         db.Column('author_id', db.Integer,
+                                   db.ForeignKey('authors.author_id'),
+                                   primary_key=True)
+                         )
+
+
+class ScientificPaper(db.Model):
+    """Scientific papers as potential matches to articles"""
+
+    __tablename__ = "papers"
+
+    id = db.Column("paper_id", db.Integer, primary_key=True)
+    doi = db.Column(db.String(64), unique=True)
+
+    title = db.Column(db.String(255, collation="utf8_general_ci"))
+
+    abstract = db.Column(db.Text(collation="utf8_general_ci"))
+
+    pubdate = db.Column(db.DateTime())
+
+    authors = db.relationship('AcademicAuthor',
+                              secondary=paper_authors,
+                              backref=db.backref('papers', lazy=True))
+
+
+
+class AcademicAuthor(db.Model):
+    """Authors of papers"""
+
+    __tablename__ = "authors"
+
+    id = db.Column("author_id", db.Integer, primary_key=True)
+
+    fullname = db.Column("fullname", db.String(255))
+
 
 
 
 class Entity(db.Model):
+    """Entity represents a named entity found in a news article"""
 
     __tablename__ = "entities"
 
