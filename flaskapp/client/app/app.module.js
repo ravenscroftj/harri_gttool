fa0182b8870e859a15ab5e84c6701093245e9732
James Ravenscroft <ravenscroftj@gmail.com>
2018-02-09 14:51:56 +0000

    Made index mobile friendly

--- flaskapp/client/app/app.module.js
+++ flaskapp/client/app/app.module.js
@@ -17,13 +17,19 @@
     .run(setGlobalState);
 
   /* @ngInject */
-  function setGlobalState($rootScope, $state, $stateParams, $transitions, $trace) {
+  function setGlobalState($rootScope, $state, $stateParams, $transitions, $mdMedia, $trace) {
     // It's very handy to add references to $state and $stateParams to the $rootScope
     // so that you can access them from any scope within your applications.For example,
     // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
     // to active whenever 'contacts.list' or one of its descendants is active.
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
+    $rootScope.isNavOpen = true; //$mdMedia('gt-sm');
+
+    $rootScope.toggleMenu = function(){
+      $rootScope.isNavOpen = !$rootScope.isNavOpen;
+    }
+
     $trace.enable('TRANSITION')
 
 
